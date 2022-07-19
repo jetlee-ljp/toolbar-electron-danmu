3970a6a0a5d0388b3dc1e424ee985a8d99b8102b
jetlee <itlijinpeng@outlook.com>
2022-07-19 11:55:24 +0800

    update main

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 const { app, BrowserWindow } = require('electron')
+
+let mainWindow;
 const createWindow = () => {
-    const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 600,
         height: 350,
         resizable: false
     })
 
-    win.loadFile("./browserWindows/html/main.html")
+    mainWindow.loadFile("./browserWindows/html/main.html")
+    mainWindow.on("closed", function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin'){
-        app.quit()
-    }
-})
+
